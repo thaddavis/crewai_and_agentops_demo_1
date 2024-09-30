f031e03b2d7e3959cf09e63cd8871c038ae7591d
tad dy <a@a.com>
2024-09-30 06:19:01 -0400

    adding agent ops

--- src/our_crew_of_agents/main.py
+++ src/our_crew_of_agents/main.py
@@ -1,6 +1,11 @@
-from src.our_crew_of_agents.crew import OurCrewOfAgents
 from dotenv import load_dotenv
 load_dotenv()
+from src.our_crew_of_agents.crew import OurCrewOfAgents
+
+import os
+import agentops
+
+agentops.init(os.getenv("AGENTOPS_API_KEY"))
 
 print("Calling our crew of agents...")
 OurCrewOfAgents().crew().kickoff()
