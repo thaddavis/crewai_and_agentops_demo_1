a84d4152758bbd48f847e960b184399bc15e2611
tad dy <a@a.com>
2024-09-30 01:21:06 -0400

    adding agents config

--- src/our_crew_of_agents/crew.py
+++ src/our_crew_of_agents/crew.py
@@ -1,14 +1,20 @@
-from crewai import Crew, Process
+from crewai import Crew, Process, Agent
 from crewai.project import CrewBase, agent, crew
 
 @CrewBase
 class OurCrewOfAgents():
 	@agent
 	def george_washington(self):
-		return Agent()
+		return Agent(
+			config=self.agents_config['george_washington'],
+    	verbose=True
+		)
 	@agent
 	def thomas_jefferson(self):
-		return Agent()
+		return Agent(
+			config=self.agents_config['thomas_jefferson'],
+    	verbose=True
+		)
 	@crew
 	def crew(self) -> Crew:
 		return Crew(
